3015514354892864d09190967ea0968f61a7d872
adpopo <hxuanpham@gmail.com>
2017-04-24 02:21:31 -0700

    heroku fixes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,19 @@
 var fs = require("fs");
 
 module.exports = function(app){
-	//use for root path
-    app.get("/", function(req, res){
+
+	//get for survey path
+    app.get("/survey", function(req, res){
     	var file = "";
-    	fs.readFile("./app/public/home.html", function(err, readFile){
+    	fs.readFile("./app/public/survey.html", function(err, readFile){
     		file += readFile;
     		res.send(file);
     	});
     });
-
-    //get for survey path
-    app.get("/survey", function(req, res){
+	//use for root path
+    app.use("/", function(req, res){
     	var file = "";
-    	fs.readFile("./app/public/survey.html", function(err, readFile){
+    	fs.readFile("./app/public/home.html", function(err, readFile){
     		file += readFile;
     		res.send(file);
     	});
